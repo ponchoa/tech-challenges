536bfdc44e436bfb25f505756a0c2d7676dfc0fb
ponchoa <benoit1.ponchon@epitech.eu>
2022-05-25 20:35:04 +0200

    Finished challenge
    Works with perspective and orthographic cameras
    Should be easy to use
    I worked an additional 30 minutes on the project to clean up a bit of the code

--- 3D/senior-3d-algorithm/Camera Challenge/Assets/Scripts/Controllers/CameraController.cs
+++ 3D/senior-3d-algorithm/Camera Challenge/Assets/Scripts/Controllers/CameraController.cs
@@ -25,6 +25,11 @@ namespace IWD.Controllers
         }
         #endregion
 
+        /// <summary>
+        /// This float is used to adjust the speed so that it is similar between zooming and moving.
+        /// </summary>
+        const float SPEED_ADJUST = 5f;
+
         [SerializeField]
         [Tooltip("The camera that will be controlled. (Main camera by default)")]
         Camera controlledCamera;
@@ -40,37 +45,71 @@ namespace IWD.Controllers
         private void Awake()
         {
             MakeInstance();
-            InitializeValues();
+            InitializeSet();
+            InitializeCamera();
+            InitializeCameraContainer();
         }
 
         private void LateUpdate()
         {
             UpdateCameraPosition();
+            UpdateCameraZoom();
         }
 
-        void InitializeValues()
+        #region Initialization
+        /// <summary>
+        /// We initialize the HashSet to be used later.
+        /// </summary>
+        void InitializeSet()
         {
             targetSet = new HashSet<CameraTarget>();
+        }
+        /// <summary>
+        /// If the camera was not set in the inspector, we use Camera.main.
+        /// We also log a warning, as it might not be expected behaviour.
+        /// </summary>
+        void InitializeCamera()
+        {
             if (controlledCamera is null)
+            {
+                Debug.LogWarning($"CameraController({name}) : No camera was set in the inspector, the main camera is used by default.", this);
                 controlledCamera = Camera.main;
-
-            // We set up a container for the camera to help with zooming.
+            }
+        }
+        /// <summary>
+        /// We set up a container for the camera to help with zooming,
+        /// as it will allow the camera to only move along its own Z-Axis regardless of its angle.
+        /// </summary>
+        void InitializeCameraContainer()
+        {
             cameraContainer = new GameObject("Camera Container");
             cameraContainer.transform.position = controlledCamera.transform.position;
             cameraContainer.transform.rotation = controlledCamera.transform.rotation;
             cameraContainer.transform.parent = controlledCamera.transform.parent;
             controlledCamera.transform.parent = cameraContainer.transform;
         }
+        #endregion
 
-        #region Position and Zoom
+        #region Position
+        /// <summary>
+        /// Updates smoothly the position of the camera container to be in the center of all targets.
+        /// </summary>
         void UpdateCameraPosition()
         {
+            // I was debating whether or not to check if any target has moved using Transform.hasChanged,
+            // but I wanted to do some performance checks, to see if using callbacks, iterating through
+            // the set once before, or simply calculating the bounds every frame was better,
+            // unfortunately I didn't have time, so I'm going on the assumption that adding an
+            // Update to each target, or processing the HashSet twice per frame was less
+            // optimized than simply recalculating bounds every frame.
             FindTargetPosition();
-            cameraContainer.transform.position = 
-                Vector3.SmoothDamp(cameraContainer.transform.position, cameraTargetPosition, ref cameraVelocity, 1f / cameraSpeed);
-            UpdateCameraZoom();
+            cameraContainer.transform.position =
+                Vector3.SmoothDamp(cameraContainer.transform.position, cameraTargetPosition, ref cameraVelocity, SPEED_ADJUST / cameraSpeed);
         }
 
+        /// <summary>
+        /// Finds the target position the container will try to reach.
+        /// </summary>
         void FindTargetPosition()
         {
             Bounds targetsBounds = new Bounds();
@@ -80,24 +119,41 @@ namespace IWD.Controllers
             }
             cameraTargetPosition = targetsBounds.center;
         }
+        #endregion
 
+        #region Zoom
+        /// <summary>
+        /// Zooms the camera out if any object is cropped, and in if it is possible.
+        /// </summary>
         void UpdateCameraZoom()
         {
             if (IsEveryTargetInFrustum())
             {
-                controlledCamera.transform.localPosition += new Vector3(0f, 0f, Time.deltaTime * cameraSpeed * 3f);
+                if (controlledCamera.orthographic)
+                    controlledCamera.orthographicSize -= cameraSpeed * Time.deltaTime;
+                controlledCamera.transform.localPosition += new Vector3(0f, 0f, Time.deltaTime * cameraSpeed * SPEED_ADJUST);
             }
+            // No 'else', because if we zoomed in too much, we correct the position before rendering to avoid spazzing.
             if (!IsEveryTargetInFrustum())
             {
-                controlledCamera.transform.localPosition -= new Vector3(0f, 0f, Time.deltaTime * cameraSpeed * 3f);
+                if (controlledCamera.orthographic)
+                    controlledCamera.orthographicSize += cameraSpeed * Time.deltaTime;
+                controlledCamera.transform.localPosition -= new Vector3(0f, 0f, Time.deltaTime * cameraSpeed * SPEED_ADJUST);
             }
         }
 
+        /// <summary>
+        /// Checks if every CameraTarget in the targetSet is entierely contained within the frustum of the camera.
+        /// </summary>
+        /// <returns>A boolean that is True if every target is in the frustum.</returns>
         bool IsEveryTargetInFrustum()
         {
             foreach (CameraTarget target in targetSet)
             {
                 Plane[] frustumPlanes = GeometryUtility.CalculateFrustumPlanes(controlledCamera);
+
+                // Unfortunately, GeometryUtility.TestPlanesAABB() checks only if at least one
+                // corner is in the planes, so we have to implement our own way.
 
                 if (target.GetRenderer() is null)
                     continue;
@@ -137,7 +193,7 @@ namespace IWD.Controllers
 
         #region Targets Controls
         /// <summary>
-        /// Adds a target that the controlled camera has to include in its frustrum.
+        /// Adds a target that the controlled camera has to include in its frustum.
         /// </summary>
         /// <param name="target">The target to add.</param>
         public void AddTarget(CameraTarget target)
